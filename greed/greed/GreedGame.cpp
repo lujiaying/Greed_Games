436dfc498128ac52b3a744738c67809f2b486801
jiaying.lu <lujiaying93@foxmail.com>
2014-12-07 12:37:56 +0800

    定义骰子、玩家的类定义，定义部分游戏系统

--- greed/greed/GreedGame.cpp
+++ greed/greed/GreedGame.cpp
@@ -1,15 +1,16 @@
 #include <stdlib.h>
+#include <iostream>
 #include "GreedGame.h"
 
+/* 骰子的构造函数 */
 Dice::Dice(){
-	valid = 1;
+	valid = true;
 	num = 0;
 }
 
 /* 投掷骰子 */
-int Dice::roll(){
+void Dice::roll(){
 	num = rand()%6 + 1;
-	return num;
 }
 
 /* 获取骰子点数 */
@@ -17,17 +18,50 @@ int Dice::getNum(){
 	return num;
 }
 
+/* 获取骰子是否可用。false代表不可用， treu代表可用*/
+bool Dice::isValid(){
+	return valid;
+}
 
+
+/* 玩家的构造函数 */
 Player::Player(){
-	for (int i = 0; i < 6; i++){
-		dices[i] = Dice();
-	}
+	dices = new Dice[dice_num];
 	current_score = 0;
 	total_score = 0;
 }
 
-/* 投掷当前所有可用的骰子。
-返回true代表可继续投掷，返回false代表本轮投掷结束。*/
-bool Player::roll(){
-	return true;
+/* 玩家的析构函数 */
+Player::~Player(){
+	delete[] dices;
+}
+
+/* 投掷当前所有可用的骰子 */
+void Player::roll(){
+	for (int i = 0; i < dice_num; i++){
+		if (dices[i].isValid){
+			dices[i].roll();
+			std::cout << "dices[" << i << "] roll: " << dices[i].getNum() << std::endl;
+		}
+	}
+}
+
+/* 给玩家本轮得分加分 */
+void Player::addCurrentScore(int score){
+	current_score += score;
+}
+
+/* 给玩家本局总得分加分 */
+void Player::addTotaltScore(int score){
+	total_score += score;
+}
+
+/* 获得玩家本轮得分 */
+int Player::getCurrentScore(){
+	return current_score;
+}
+
+/* 获得玩家本局总得分 */
+int Player::getTotalScore(){
+	return total_score;
 }
--- greed/greed/HelloWorld.cpp
+++ greed/greed/HelloWorld.cpp
@@ -2,39 +2,19 @@
 #include "GreedGame.h"
 using namespace std;
 
-class CRectangle {
-
-	int *width, *height;
-public:
-	CRectangle(int, int);
-	~CRectangle();
-	int area(void) { return (*width * *height); };
-};
-
-CRectangle::CRectangle(int a, int b) {
-	width = new int;
-	height = new int;
-	*width = a;
-	*height = b;
-}
-
-CRectangle::~CRectangle(){
-	delete width;
-	delete height;
-}
-
 int main()
 {
-	/**
-	CRectangle rect(3, 4);
-	CRectangle rectb(5, 6);
-	cout << "rect area: " << rect.area() << endl;
-	cout << "rectb area: " << rectb.area() << endl;
-	**/
-
-	Dice dice;
+	/*Dice dice;
 	for (int i = 0; i < 10; i++) {
 		cout << i+1 << "st roll: " << dice.roll() << endl;
+	}*/
+
+	Player player;
+	char continue_roll = 'y';
+	while (continue_roll != 'n') {
+		cout << endl << "type y to continue, n to end: ";
+		cin >> continue_roll;
+		player.roll();
 	}
 	getchar();
 }
